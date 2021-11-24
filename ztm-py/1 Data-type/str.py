3b16ce5ea58569b60d1c788348fd6790e8ee0e7c
hiteshpakhan <hitesh5pakhan@gmail.com>
2021-11-24 20:20:29 +0530

    a

--- ztm-py/1 Data-type/str.py
+++ ztm-py/1 Data-type/str.py
@@ -10,9 +10,29 @@ kesrg rb vr
 ekrjv j fv
 se'''))         # it will also give you the type str
 
+
 # 2 string concatination
 # you can also perforn some experiment on the str  
 first_name = "hitesh"
 last_name = "pakhan"
 full_name = first_name + " " + last_name
 print(full_name)
+
+
+# 3 string indexes:-
+big_string = "ABCDFGA566@GMAIL.COM"
+#             12345678901234567890
+index4 = big_string[4]
+print(index4)       #it will give the output f
+
+
+# 4 string indexes [start:stop]
+index4_11 = big_string[4:11]
+print(index4_11)        #it will give the output FGA566@
+
+
+# 5 string indexes [start:stop:stepover]
+index4_11_1 = big_string[4:11:1]
+print(index4_11_1)        #it will give the output FGA566@
+
+
