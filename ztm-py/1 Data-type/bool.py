f2db61803c30a9dcf25ab0bdc4cd6bd8baecc976
hiteshpakhan <hitesh5pakhan@gmail.com>
2021-11-27 09:30:40 +0530

    a

---
+++ ztm-py/1 Data-type/bool.py
@@ -0,0 +1,10 @@
+# bool      
+# bool can be ether true or false 
+
+name = "hitesh"
+is_cool=bool(1)
+is_not_cool=bool(0)
+print(is_cool)          # it will give the output true
+print(is_not_cool)      # it will give the output false
+
+print(bool("false"))   #it will give the output true because it check is something present or not not cheack the whats in that the string
