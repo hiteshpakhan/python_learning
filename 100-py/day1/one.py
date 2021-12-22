b7c0fa1ebffc70e522b3b548ce6c9619ce28031b
hiteshpakhan <hitesh5pakhan@gmail.com>
2021-12-22 19:53:26 +0530

    a

--- 100-py/day1/one.py
+++ 100-py/day1/one.py
@@ -1,3 +1,8 @@
 print('''day 1 - python print function
 the function is declared like this
-print('print what to print')''')
+print('print what to print')''')    #this is the long string
+
+print(len("hi there"))      #this will tell the number of characters in the string
+
+#Remember input always takes the data in the form of string (str)
+
