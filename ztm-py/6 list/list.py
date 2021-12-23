1c875b1c6c1ca7d99dc90c06ec508485293525c1
hiteshpakhan <hitesh5pakhan@gmail.com>
2021-12-23 21:12:39 +0530

    a

--- ztm-py/6 list/list.py
+++ ztm-py/6 list/list.py
@@ -1,1 +1,13 @@
 # list is a data type which is collection of objects that can be of any type
+# in other programming languages you might heard the word arrays , list is just like that
+
+li = [1, "cAT", True, "dog"]
+
+print(li)
+# op---     [1, 'cAT', True, 'dog']
+
+print(li[2])
+# op---     True
+
+print(li[0])
+# op---     1
