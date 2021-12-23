94c1f6b38d6a5c361313c66b38c5db29d80addb4
hiteshpakhan <hitesh5pakhan@gmail.com>
2021-12-23 18:59:38 +0530

    a

--- 100-py/day1/one.py
+++ 100-py/day1/one.py
@@ -1,8 +1,10 @@
+# Remember input always takes the data in the form of string (str)
+# len()
+
+
+
 print('''day 1 - python print function
 the function is declared like this
 print('print what to print')''')    #this is the long string
 
 print(len("hi there"))      #this will tell the number of characters in the string
-
-# Remember input always takes the data in the form of string (str)
-# len()
--- 100-py/day3/exercise.py
+++ 100-py/day3/exercise.py
@@ -1,17 +1,27 @@
+# if
+# elif
+# else
+# and
+# or
+# not
+
+
+
 # height = float(input("enter the height"))
 # weight = float(input("enter the weight"))
 # bmi = round(weight / height ** 2)
 # print(bmi)
 # if bmi < 18.5:
 #     print("underwight")
-# elif bmi < 25:
+# elif bmi > 18.5 and bmi < 25:
 #     print("Normal weight")
-# elif bmi < 30:
+# elif bmi > 25 and bmi < 30:
 #     print("overweight")
-# elif bmi < 35:
+# elif bmi > 30 and bmi < 35:
 #     print("obese")
 # else:
 #     print("clinically obese")
+
 
 
 # year = int(input("enter the year you want to check : "))
