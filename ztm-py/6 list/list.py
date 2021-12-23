3e0fd1d6695c56a766fb94e520809f5067123e81
hiteshpakhan <hitesh5pakhan@gmail.com>
2021-12-23 21:17:41 +0530

    a

--- ztm-py/6 list/list.py
+++ ztm-py/6 list/list.py
@@ -2,12 +2,13 @@
 # in other programming languages you might heard the word arrays , list is just like that
 
 li = [1, "cAT", True, "dog"]
+print(li)           # op---     [1, 'cAT', True, 'dog']
+print(li[2])        # op---     True
 
-print(li)
-# op---     [1, 'cAT', True, 'dog']
-
-print(li[2])
-# op---     True
-
-print(li[0])
-# op---     1
+# List Slicing
+amazon_cart = [
+    "notebook",
+    "sunglasses",
+    "toys",
+    "graps"
+]
