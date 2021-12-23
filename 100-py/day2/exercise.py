108ae1620b95547f3441b82aa16f555caec35838
hiteshpakhan <hitesh5pakhan@gmail.com>
2021-12-23 13:03:44 +0530

    a

---
+++ 100-py/day2/exercise.py
@@ -0,0 +1,10 @@
+bill = float(input("what is the total bill : "))
+tip = int(input("how much percent tip you like to give : "))
+people = int(input("in how many people you like to split the bill : "))
+bill_with_tip = (tip / 100 * bill) + bill 
+print("total bill is : " , round(bill_with_tip))
+print(f"bill for every person : {round(bill_with_tip / people)}")
+
+# round()
+# f"string {variable}"
+# input
