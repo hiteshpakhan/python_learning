d27b279f455fe52a3eba72405472507acb3bf2e3
hiteshpakhan <hitesh5pakhan@gmail.com>
2021-12-24 13:07:45 +0530

    a

--- ztm-py/6 list/list.py
+++ ztm-py/6 list/list.py
@@ -18,3 +18,44 @@ amazon_cart = [
 # unlike the strings you can here change the data inside the list by assigning the new values
 amazon_cart[0] = "new value" 
 print(amazon_cart)
+
+
+# matrix : matrix is just like the list of lists
+matrixone = [
+    [1,2,3],
+    [4,5,6],
+    [5,7,3]
+]
+print(matrixone[0][2])
+print(len(matrixone))
+
+
+# methods with list
+basket = [1,2,3,4,5]
+basket.append(100)
+print(basket)
+
+basket.insert(3, 100)
+print(basket)
+
+basket.extend([3, 1, 5])
+print(basket)
+
+basket.pop()
+print(basket)             #it will remove the last element from the list
+
+basket.pop(6)
+print(basket)             #it will remove the data from the list at the index you provied to it
+
+basket.remove(4)
+print(basket)             #it will remove the number from the list
+
+basket.clear()
+print(basket)             #it will empty the list
+
+
+# list methods 2
+
+basket2 = [1,2,3,4,5,6]
+print(basket2.index(4))   #it will give the index of the number you provide inside the brackets
+print(basket2.index(3, 1, 4))   #you can also give start and stope index for looking
