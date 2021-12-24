10d7f9c691809b68dae16425e39ff6768ab11e89
hiteshpakhan <hitesh5pakhan@gmail.com>
2021-12-24 18:45:24 +0530

    a

--- ztm-py/6 data structure/list.py
+++ ztm-py/6 data structure/list.py
@@ -1,4 +1,4 @@
-# list
+# list []
 # .append()
 # .insert()
 # .extend()
@@ -108,4 +108,8 @@ print(new_sentence)
 
 
 # list unpaking
-a,b,c,*d = [1,2,3,4,5,6,7,8,9]
+a,b,*c,d = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]
+print(a)
+print(b)
+print(c)
+print(d)
