a663bc3a6c05a612f5b398e35e98ca7ce1d61d5a
hiteshpakhan <hitesh5pakhan@gmail.com>
2021-12-25 14:55:35 +0530

    a

---
+++ ztm-py/6 data structure/set.py
@@ -0,0 +1,4 @@
+# set {}
+# set is an unordered collection of an unique elements
+my_set = {1,1,2,3,4,4,5,6,6,7,7,8,8}
+print(my_set)   #it will give the output {1, 2, 3, 4, 5, 6, 7, 8} it will automatically remove all the duplecate elements
--- ztm-py/6 data structure/tuple.py
+++ ztm-py/6 data structure/tuple.py
@@ -1,8 +1,14 @@
 # tuple ()
+# .count()
+# .index()
+
+
+
+# tuple ()
 # tuple is similer to the list 
 # you can not perform sort or reverse like operations on the tuple
 
-tuple_one = (1,2,3,4,5)
+tuple_one = (1,2,3,4,5,5,5)
 print(tuple_one)
 print(tuple_one[1])
 print(tuple_one[1:3])
@@ -13,3 +19,13 @@ print(b)
 print(other)
 
 # tuple_one[1] = 56     #it will give an error because tuple not support item assignment 
+
+print(5 in tuple_one)
+
+# tuple methods
+
+# .count()
+print(tuple_one.count(5))       #it will count how many times the value has occoured in the tuple
+
+# .index()
+print(tuple_one.index(5))       #it will give the index where given value occore first
