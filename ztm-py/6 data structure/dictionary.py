1f0cf7c74971ed0d56e52410ddb47aa08200836c
hiteshpakhan <hitesh5pakhan@gmail.com>
2021-12-25 11:46:06 +0530

    'a'

--- ztm-py/6 data structure/dictionary.py
+++ ztm-py/6 data structure/dictionary.py
@@ -1,5 +1,6 @@
 # dictionary {dict}
 # dictonary is a collection of key and value it is just like the object in the javascript and it stores the data inside the querly brackets
+# imp :- dictionary is not unordered it is not like the list that remember the line in which they inserted if the dictionary is very big we will not abele to access the indexing properly
 
 dictonary_one = {
     "key1" : 1,
@@ -24,5 +25,31 @@ print(dictonary_one.get("key5", 35))    #now when the value not present it will 
 
 # in
 print("key1" in dictonary_one)              #it willcheck in all the dictonary if the value is present
+
+# .keys()
 print("key1" in dictonary_one.keys())       #it will check if the value present in keys of the dictonary
+
+# .values()
 print("key1" in dictonary_one.values())     #it will check if the value present in the value in the dictonary
+
+# .items()
+print(dictonary_one.items())
+
+# .copy()
+dictonary_two = dictonary_one.copy()
+print(dictonary_two)                        #it will copy the dictonary_one data into the dictonary_two 
+
+# .clear()
+dictonary_one.clear()
+print(dictonary_one)        #it will delete all the keys and values from the dictonary
+
+# .pop()
+print(dictonary_two.pop("key2"))            #it will remove the key & value of the key you give
+print(dictonary_two)
+
+# .popitem()
+print(dictonary_two.popitem())              #it will randomely pop any item from the dictionary
+print(dictonary_two)
+
+# .update()
+print(dictonary_two.update({"key1" : 11}))  #it is used to update the value of the key
