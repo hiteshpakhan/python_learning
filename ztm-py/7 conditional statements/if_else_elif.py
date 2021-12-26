e67af3aafac01713c3c1a6eb6f9ec7cd0248b2a6
hiteshpakhan <hitesh5pakhan@gmail.com>
2021-12-26 08:43:14 +0530

    a

---
+++ ztm-py/7 conditional statements/if_else_elif.py
@@ -0,0 +1,26 @@
+# if
+# elif
+# else
+# and 
+# or
+# not
+# ternary operator 
+
+a = 100
+if a == 0 and a != 0:
+    print("this is if")
+elif not a > 0:
+    print("this is elif")
+else:
+    print("this is else")
+
+
+
+
+# ternary operator 
+c = True
+
+b = "value1" if c == False else "value2"
+print(b)
+# this is second type of if else which can be used like this
+
