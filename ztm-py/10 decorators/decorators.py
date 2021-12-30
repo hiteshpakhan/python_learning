2f5a0a70e84c2e7789d205192c22c82f1d46b358
hiteshpakhan <hitesh5pakhan@gmail.com>
2021-12-30 15:07:53 +0530

    a

---
+++ ztm-py/10 decorators/decorators.py
@@ -0,0 +1,4 @@
+# decorators have the @ sign at the starting and name next to it 
+# @classmethod and @staticmethod are the two decorators that we seen before
+# decorators supercharge our functions
+
---
+++ ztm-py/10 decorators/higher_order_functions.py
@@ -0,0 +1,12 @@
+# higher order functions
+# a higher order function can ether be function that accept another function 
+def greet(func):
+    func()
+
+# or the higher order functions can be those that returns another function
+def greet2():
+    def func2():
+        return "something"
+    return func2
+
+# means a higher ordered function can accept or return another function 
