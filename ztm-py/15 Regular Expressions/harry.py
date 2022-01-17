77b69040d9c24f32351037b52d8bc7a500922e5e
hiteshpakhan <hitesh5pakhan@gmail.com>
2022-01-17 22:52:01 +0530

    a"

--- ztm-py/15 Regular Expressions/harry.py
+++ ztm-py/15 Regular Expressions/harry.py
@@ -6,7 +6,16 @@ mystr = '''@kjbkb. 0 Groups. kjbkb.
 RecordingsSmuleGuitar!Magic PianoAutoRap. 
 kjbkb has no recordings.'''
 
+
 pattern1 = re.compile(r"Recording")
+# .finditer()
 a = pattern1.finditer(mystr)
 print(a)
 # output:- <callable_iterator object at 0x000002C3138ECF10>
+
+for i in a:
+    print(i)
+# <re.Match object; span=(27, 36), match='Recording'>
+# <re.Match object; span=(76, 85), match='Recording'> 
+
+# as you can see that there an object that holds the data in group
