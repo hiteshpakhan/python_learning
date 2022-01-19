fd855a7bfade1cc331a104b36ce2032dda0b0169
hiteshpakhan <hitesh5pakhan@gmail.com>
2022-01-19 18:53:59 +0530

    a

--- ztm-py/15 Regular Expressions/ztm/exercise.py
+++ ztm-py/15 Regular Expressions/ztm/exercise.py
@@ -1,9 +1,10 @@
 import re
 
-password = "Luci@714"
+password = "Luci@71421"
 
-pass_pattern = re.compile(r"[A-Za-z0-9%$#@]{8,}\d")
+pass_pattern = re.compile(r"[A-Za-z0-9%$#@]{8,}\d")     #it will check 1> more than 8 characters 2> end with number 3>can also have symboals
 
 check = pass_pattern.fullmatch(password)
 
 print(check)
+# output:-      <re.Match object; span=(0, 10), match='Luci@71421'>
