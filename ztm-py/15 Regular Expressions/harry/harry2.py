591edfee0703444c8f20a3f40d8055df9802c005
hiteshpakhan <hitesh5pakhan@gmail.com>
2022-01-19 12:56:51 +0530

    a

--- ztm-py/15 Regular Expressions/harry/harry2.py
+++ ztm-py/15 Regular Expressions/harry/harry2.py
@@ -1,10 +1,10 @@
 import re
-para = '''@kjbkb. 0 Groups. kjbkb. 
+para = '''@kjbkb. 0 Groups. kjbkbkjbkb. 
 2Recordings0Songs0Favorites0Followers0Following. 
-RecordingsSmuleGuitar!Magic PianoAutoRap. 
+RecordingsSmuleGuitar!Maginnnnnc PianoAutoRap. 
 kjbkb has no recordings.'''
 
-# .
+# . any character
 patt = re.compile(r".")     #by using . you are saying you want any characters
 obj_store = patt.finditer(para)
 for i in obj_store:
@@ -40,18 +40,67 @@ for l in obj_store4:
     print(l)        #output:- <re.Match object; span=(138, 142), match='ings'>
 
 
-# *
-print("****")
+# * zero or more than zero
 patt5 = re.compile(r"in*")      #here you are saying that you want g and if s comes after that zore or more than zero times
-obj_stroe5 = patt5.finditer(para)
-for m in obj_stroe5:
+obj_store5 = patt5.finditer(para)
+for m in obj_store5:
     print(m)
-    # output:-
+
+# output:-
 # <re.Match object; span=(33, 35), match='in'>
 # <re.Match object; span=(49, 50), match='i'>
 # <re.Match object; span=(70, 72), match='in'>
 # <re.Match object; span=(82, 84), match='in'>
 # <re.Match object; span=(93, 94), match='i'>
 # <re.Match object; span=(101, 102), match='i'>
-# <re.Match object; span=(105, 106), match='i'>
-# <re.Match object; span=(138, 140), match='in'>
+# <re.Match object; span=(101, 107), match='innnnn'>
+# <re.Match object; span=(110, 111), match='i'>
+# <re.Match object; span=(143, 145), match='in'>
+
+
+# + one or more than one
+patt6 = re.compile(r"in+")
+obj_store6 = patt6.finditer(para)
+for n in obj_store6:
+    print(n)
+# output:-
+# <re.Match object; span=(33, 35), match='in'>
+# <re.Match object; span=(70, 72), match='in'>
+# <re.Match object; span=(82, 84), match='in'>
+# <re.Match object; span=(101, 107), match='innnnn'>
+# <re.Match object; span=(143, 145), match='in'>
+
+
+# {} exactily the specified number of occurrence
+patt7 = re.compile(r"in{5}")
+obj_store7 = patt7.finditer(para)
+for o in obj_store7:
+    print(o)
+# output:-
+# <re.Match object; span=(101, 107), match='innnnn'>
+
+
+
+# () capture and group
+patt8 = re.compile(r"(kjbkb){2}")   #it will check hole kjbkb 2 times
+obj_store8 = patt8.finditer(para)
+for p in obj_store8:
+    print(p)
+# output:-
+# <re.Match object; span=(18, 28), match='kjbkbkjbkb'>
+
+
+
+# | either or
+print("****")
+patt9 = re.compile(r"Re|kj")
+obj_store9 = patt9.finditer(para)
+for q in obj_store9:
+    print(q)
+# output:-
+# <re.Match object; span=(1, 3), match='kj'>
+# <re.Match object; span=(18, 20), match='kj'>
+# <re.Match object; span=(23, 25), match='kj'>
+# <re.Match object; span=(32, 34), match='Re'>
+# <re.Match object; span=(81, 83), match='Re'>
+# <re.Match object; span=(129, 131), match='kj'>
