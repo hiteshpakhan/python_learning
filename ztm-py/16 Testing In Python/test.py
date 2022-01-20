6f46f223292cc6e395c6523982edefec1c25f640
hiteshpakhan <hitesh5pakhan@gmail.com>
2022-01-20 13:58:25 +0530

    a

--- ztm-py/16 Testing In Python/test.py
+++ ztm-py/16 Testing In Python/test.py
@@ -1,1 +1,11 @@
 import unittest
+import main
+
+class TestMain(unittest.TestCase):
+    def test_do_stuff(self):
+        test_param = 10
+        result = main.do_stuff(test_param)
+        self.assertEqual(result, 15)    #it will check if the result is equal to the excepted value
+
+
+unittest.main()
