6e11d1f7d2da0c4421212aee96610af1c3c88093
hiteshpakhan <hitesh5pakhan@gmail.com>
2022-01-21 10:25:58 +0530

    a

---
+++ ztm-py/raw/two.py
@@ -0,0 +1,14 @@
+import os
+
+
+# os.system("tasklist")
+# print("________________________________________________________")
+
+# os.system("ping google.com")
+# print("________________________________________________________")
+
+# print(os.name)      # "nt" = windows
+
+
+
+# 
