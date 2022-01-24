83d2ef1ce8b28fc5c4894295a69140e095a8dd30
hiteshpakhan <hitesh5pakhan@gmail.com>
2022-01-24 21:02:55 +0530

    a

--- ztm-py/17 Scripting with Python/one/exercise.py
+++ ztm-py/17 Scripting with Python/one/exercise.py
@@ -1,1 +1,31 @@
 # jpg to png convertor
+
+
+# os.path.exists()
+# os.makedirs()
+
+
+from distutils.command.clean import clean
+import sys  #sys help you to get the parameters with the running the file
+import os   
+from PIL import Image
+
+img_folder = sys.argv[1]
+output_folder = sys.argv[2]
+
+print(img_folder, output_folder)
+
+# now we have to check if the new foldfer is exist if not then we have to create it
+# by importing the os module you can check if the specific path present or not #there is another module that you can use to check if the file is present or not that is pathlib
+if not os.path.exists(output_folder): #it will return true if it exist or it will return the false # also we assign the not at the front the condition if it return the flase then only it will execute
+    os.makedirs(output_folder)        # it will create the new folder name new
+    print(f"the new file {output_folder} has been created")
+
+for each_file in os.listdir(img_folder):
+    if each_file == "astro.jpg":    #it will not contain the astro img
+        continue    
+    img = Image.open(f"{img_folder}{each_file}")
+    clean_name = os.path.splitext(each_file)[0]
+    img.save(f"{output_folder}{clean_name}.png", "png")
+    print("all done")
+
---
+++ ztm-py/17 Scripting with Python/one/main2.py
@@ -0,0 +1,4 @@
+from PIL import Image
+
+img1 = Image.open("Pokedex/charmander.jpg")  # it is ok if we do not give the ./ at the start of the path
+img1.show()
