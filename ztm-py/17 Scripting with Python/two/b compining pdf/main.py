6a98eb56a51cadc229f74786990d64bb48f7785c
hiteshpakhan <hitesh5pakhan@gmail.com>
2022-01-25 16:42:56 +0530

    a

--- ztm-py/17 Scripting with Python/two/b compining pdf/main.py
+++ ztm-py/17 Scripting with Python/two/b compining pdf/main.py
@@ -5,7 +5,18 @@ inputs = sys.argv[1:]   # here at the index 0 always the filename
 # by doing [1:] it will take all the argument we pass into a list
 
 def pdf_combiner(pdf_list):
+    merger = PyPDF2.PdfFileMerger()
     for pdf in pdf_list:
         print(pdf)
+        merger.append(pdf)
+    merger.write("combined.pdf")
+
 
 pdf_combiner(inputs)
+
+# to run this file :
+    # you have to go to the folder location and open the terminal
+    # and type the following command
+    # python main.py dummy.pdf twopage.pdf wtr.pdf
+
+# it will combine all the pdf you give to it 
