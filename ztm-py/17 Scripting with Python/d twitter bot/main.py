5fd4a8b773afdb7b40c8ecd8c83c2bdafe990703
hiteshpakhan <hitesh5pakhan@gmail.com>
2022-01-27 16:29:43 +0530

    a

--- ztm-py/17 Scripting with Python/d twitter bot/main.py
+++ ztm-py/17 Scripting with Python/d twitter bot/main.py
@@ -1,1 +1,19 @@
 import tweepy
+
+auth = tweepy.OAuthHandler("bLsisgRN4QoBYQRqj5Z6qEb2T", "x0y8Cj8Ft6Qqlba9iZXUMpkIqi2ZHfrEUlS6xXWjuH1mMRXSVf")
+auth.set_access_token("909984739301326848-DsZHUr8ZTONeQgoasNBH7kqrB1vT2bN", "yqiFB2ZtCap5i55VNIwtQtR3VzJqrHOUOGpwK8QsDQgTO")
+
+api = tweepy.API(auth)
+
+public_tweets = api.home_timeline()
+for tweet in public_tweets:
+    print(tweet.text)
+
+
+user = api.me()
+print(user.name)
+
+print(user.screen_name)
+
+print(user.followers_count)
+
