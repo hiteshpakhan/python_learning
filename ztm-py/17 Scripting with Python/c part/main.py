2314c25d9f4151ab3f212134694f99cb3bdc9cd3
hiteshpakhan <hitesh5pakhan@gmail.com>
2022-01-27 15:09:25 +0530

    a

--- ztm-py/17 Scripting with Python/c part/main.py
+++ ztm-py/17 Scripting with Python/c part/main.py
@@ -1,6 +1,6 @@
-from distutils.log import error
-from urllib import request
 import requests
+import hashlib
+import sys
 
 # if you get the <Response [400]> then that means you get the error 
 # now we get the <Response [200]>
@@ -11,11 +11,33 @@ def request_api_data(query_char):
     res = requests.get(url)
     if res.status_code != 200:
         raise RuntimeError(f"err facing {res.status_code}, check the api and try again")
+    return res
+
+
+def get_password_leaks_count(hashes, hash_to_check):
+    hashes = (line.split(":") for line in hashes.text.splitlines())
+    for h, count in hashes:
+        if h == hash_to_check:
+            print("h=",h,"count=",count)
+            return count
+    return 0 # if it not return th count it will return the 0 
+
+
+def pwned_api_check(password):
+    sha1_password = hashlib.sha1(password.encode("utf-8")).hexdigest().upper()
+    first5char, remaningchar = sha1_password[:5], sha1_password[5:]
+    response = request_api_data(first5char) 
+    # print(response)
+    return get_password_leaks_count(response, remaningchar)
+
+def main(a):
+    for password in a:
+        count = pwned_api_check(password)
+        if count:
+            print(f"{password} was found {count} times.... you shoud probably change your password")
+        else:
+            print(f"{password} not found, good work")
+    return "done"
     
-
-def check_pwned_api(password):
-    # check password if it exist in the api responce 
-    pass
-
-
-request_api_data("CBFDA")
+if __name__ == "__main__":
+    sys.exit(main(sys.argv[1:]))    #sys.exit is very important command it will take our control back to the cammand line after executes the program
