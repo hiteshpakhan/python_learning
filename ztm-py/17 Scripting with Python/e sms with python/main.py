e6cb2cdb0994690ddaad1e67731f017237308984
hiteshpakhan <hitesh5pakhan@gmail.com>
2022-01-28 10:33:02 +0530

    a

---
+++ ztm-py/17 Scripting with Python/e sms with python/main.py
@@ -0,0 +1,7 @@
+# twilio : hitesh5pakhan@gmail.com
+# password : Luci@7142128354249
+# link : https://console.twilio.com/?frameUrl=%2Fconsole%3Fx-target-region%3Dus1&newCustomer=true
+# accout name : Luci@714SMS
+# link: https://console.twilio.com/?frameUrl=%2Fconsole%3Fx-target-region%3Dus1&newCustomer=true
+# account sid : ACceea98b33845c6e787802ecc3f38077c
+# account token : 20aa601f781471687f7a7096a54ad638
