fa7fc411dd12144f2dd4c508aafc868d70ced1a0
hiteshpakhan <hitesh5pakhan@gmail.com>
2022-01-29 20:10:08 +0530

    a

--- ztm-py/20 AutomationTesting/main.py
+++ ztm-py/20 AutomationTesting/main.py
@@ -3,3 +3,28 @@
 # pip install selenium
 
 from selenium import webdriver
+
+chrome_browser = webdriver.Chrome(".\chromedriver")
+
+# print(chrome_browser) # it will open the chrome browser
+
+chrome_browser #it will open th echrome browser
+
+chrome_browser.maximize_window() #it will maximize the window
+
+chrome_browser.get("https://reactjs.org/docs/getting-started.html")
+
+print("Getting Started – React" == chrome_browser.title)    #output:- True       #it will check the title of the browser and give it to you
+
+assert chrome_browser.title in "jghvjkvkvjkuhvjkuvbjkhvb"   #assert - if the condition is false then it will give the error and callout of the console
+
+
+
+
+
+
+
+
+
+
+
