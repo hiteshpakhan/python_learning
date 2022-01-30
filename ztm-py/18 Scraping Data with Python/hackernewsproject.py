bd75c4777b5d3c35435e332e0c9c9b858d0a8c50
hiteshpakhan <hitesh5pakhan@gmail.com>
2022-01-30 18:13:20 +0530

    a

--- ztm-py/18 Scraping Data with Python/hackernewsproject.py
+++ ztm-py/18 Scraping Data with Python/hackernewsproject.py
@@ -1,6 +1,18 @@
+from os import link
+from turtle import title
 import requests
 from bs4 import BeautifulSoup
 
 res = requests.get("https://news.ycombinator.com/")
 collected_html = BeautifulSoup(res.text, "html.parser")
-print(collected_html.select(".titlelink")[0])
+news_title = collected_html.select(".titlelink")
+votes = collected_html.select(".score")
+
+def create_custom_hacker_news(links, votes):
+    h_n_list = []
+    for index, item in enumerate(links):
+        title = links[index].getText()
+        h_n_list.append(title)
+    return h_n_list
+
+print(create_custom_hacker_news(news_title, votes))
