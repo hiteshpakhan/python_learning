aea7eadc2d9c9bd62833b7da8b3e21a749923a06
hiteshpakhan <hitesh5pakhan@gmail.com>
2022-01-30 22:21:21 +0530

    a

--- ztm-py/18 Scraping Data with Python/h_n_project.py
+++ ztm-py/18 Scraping Data with Python/h_n_project.py
@@ -11,8 +11,11 @@ votes = collected_html.select(".score")
 def create_custom_hacker_news(links, votes):
     h_n_list = []
     for index, item in enumerate(links):
-        title = links[index].getText()
-        h_n_list.append(title)
+        title = links[index].getText()  #it will get the text inside the element
+        href = links[index].get("href", None)   #it will catch the link of the news 
+        points = int(votes[index].getText().replace(" points", ""))
+        print(points)
+        h_n_list.append({"title" : title, "href" : href})
     return h_n_list
 
 print(create_custom_hacker_news(news_title, votes))
