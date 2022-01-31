b7a078386f2e5ae426dae57d98522666ca0c3b0b
hiteshpakhan <hitesh5pakhan@gmail.com>
2022-01-31 12:06:01 +0530

    a

--- ztm-py/20 AutomationTesting/main.py
+++ ztm-py/20 AutomationTesting/main.py
@@ -12,20 +12,32 @@ chrome_browser #it will open th echrome browser
 
 chrome_browser.maximize_window() #it will maximize the window
 
-chrome_browser.get("https://reactjs.org/docs/getting-started.html")
+chrome_browser.get("http://texttospeechrobot.com/")
 
-print("Getting Started – React" == chrome_browser.title)    #output:- True       #it will check the title of the browser and give it to you
+assert "Text To Speech Online" in chrome_browser.title   #assert - if the condition is false then it will give the error and callout of the console
 
-# assert chrome_browser.title in "jghvjkvkvjkuhvjkuvbjkhvb"   #assert - if the condition is false then it will give the error and callout of the console
+# assert "Text To Speech Online" in chrome_browser.body   #error : it has no attribute called body
 
 # there is a probleme here we can not get the body section 
 # to get the body section data you have to use the selectors
 # you can read about selectors in following link
 # http://allselenium.info/python-selenium-commands-cheat-sheet-frequently-used/
 
+# to select the button from page
+button_element = chrome_browser.find_element_by_id("speak_button")
+print(button_element, "############################")   #it will grab the button
+# to select the element by classname
+# chrome_browser.find_element_by_class_name("classname")
 
-# we can grab items inside the page by using the selectrs
-element = chrome_browser.find_element_by_class_name("css-19nz7r4")  #ti will grabe the element by the class name as we said
-print(element)  #you can see the button that we grab  # but if you want to grab the info about that button you can do following
-print(element.get_attribute("innerHtml")) 
-#we will do the automation after some time
+print(button_element.get_attribute("innerHTML"), "* well our button dont have any innerHTMl *")
+
+assert "speak_button" in chrome_browser.page_source     #it will check hole page source 
+
+text_area = chrome_browser.find_element_by_id("text_box")
+# now we have to check the textarea is empty
+text_area.clear()   #it wiull clear the textarea
+text_area.send_keys("hee there how are you doing, hope you are doing fine")
+
+
+
+# chrome_browser.close()  #it will close the browser that we open
