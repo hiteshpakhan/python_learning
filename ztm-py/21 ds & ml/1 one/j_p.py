ec48570237edf278567513c70c9a77afaec5ad24
hiteshpakhan <hitesh5pakhan@gmail.com>
2022-02-06 16:01:52 +0530

    a

--- ztm-py/21 ds & ml/1 one/j_p.py
+++ ztm-py/21 ds & ml/1 one/j_p.py
@@ -5,6 +5,6 @@ data_collection = pandas.read_csv("data.csv")
 # print(data_collection["Age"].head(10))   #it will print only data realted to the age
 # print(data_collection[data_collection["Nationality"] == "India"])
 
-new_data = pandas.DataFrame(data_collection, columns=["Name","Wage","Value"])   #you can choose multiple columns
+new_data = pandas.DataFrame(data_collection, columns=["Name","Wage","Value"])   #.DataFrame() is a method that helps you to choose multiple columns
 # print(new_data)
 
