a5a4d521f15f5376e30aba39395249347928a949
Hitesh Pakhan <77678075+hiteshpakhan@users.noreply.github.com>
2023-09-03 22:58:45 +0530

    Update new.py

--- infytq-py/new.py
+++ infytq-py/new.py
@@ -3,3 +3,5 @@ it is the set of programs and other info used by a computer to perform a specifi
 system software which provides the platform for other software
 
 device drivers : 
+
+# you can type note
